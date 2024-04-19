6f9644a1fa3c377f6471c915bb303cf87c0b963b
ucperry <p.wolfe@icloud.com>
2024-04-19 11:15:05 -0400

    more app changes

--- Firebase Mobile App/index.js
+++ Firebase Mobile App/index.js
@@ -2,3 +2,12 @@
 Challenge:
 Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 */
+
+let items=[]
+const inputFieldEl = document.getElementById("input-field")
+const addBtnEl = document.getElementById("add-button")
+
+addBtnEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    console.log(inputValue)
+})
