4ce149c4167e53eac23acaacefc74f5c92b8b954
perry <p.wolfe@icloud.com>
2024-04-23 21:17:07 -0400

    list Items able to be removed and Boolean for when database does not exist

--- Firebase Mobile App/index.js
+++ Firebase Mobile App/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL:"https://realtime-database-2b431-default-rtdb.firebaseio.com/"
@@ -22,18 +22,24 @@ addButtonEl.addEventListener("click", function() {
 })
 
 onValue(shoppingListInDB, function(snapshot) {
-    let itemsArray = Object.entries(snapshot.val())
+    if (snapshot.exists()){
+        let itemsArray = Object.entries(snapshot.val())
     
-    clearShoppingListEl()
-    
-    for (let i = 0; i < itemsArray.length; i++) {
-        let currentItem = itemsArray[i]
-       
-        let currentItemID = currentItem[0]
-        let currentItemValue = currentItem[1]
+        clearShoppingListEl()
         
-        appendItemToShoppingListEl(currentItem)
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+           
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToShoppingListEl(currentItem)
+        }
+    } else {
+        shoppingListEl.innerHTML = "No Items Needed... Yet"
     }
+    
+   
 })
 
 function clearShoppingListEl() {
@@ -45,12 +51,17 @@ function clearInputFieldEl() {
 }
 
 function appendItemToShoppingListEl(item) {
-    //shoppingListEl.innerHTML += `<li>${itemValue}</li>`
-     let itemID = item[0]
-     let itemValue = item[1]
+    let itemID = item[0]
+    let itemValue = item[1]
     let newEl = document.createElement("li")
 
     newEl.textContent = itemValue
+
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}` )
+
+        remove(exactLocationOfItemInDB)
+    })
 
     shoppingListEl.append(newEl)
 }
