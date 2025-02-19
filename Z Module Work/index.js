cede67cbcc44d80ca6618fab6e070881a2d85a5d
ucperry <146903575+ucperry@users.noreply.github.com>
2025-02-19 16:57:18 -0500

    Delete Z Module Work directory

--- Z Module Work/index.js
+++
@@ -1,8 +0,0 @@
-// Challenge:
-// Round the price in the button down to two decimal places.
-// Don't know which method to use? Google it!
-
-const totalPrice = "420.69235632455"
-const btn = document.getElementById("purchase-btn")
-btn.textContent = `Buy €${ Number(totalPrice).toFixed(2) }`
-
